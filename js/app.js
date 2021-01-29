6ee0fb35dcbcdfca082169fa27772a12b6bb22c3
Oluwafunto <75193130+Oluwafunto@users.noreply.github.com>
2021-01-28 21:06:09 -0500

    Personal composition

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let myName = "A. name";
+console.log(myName);
+
+myName = "some variable, different name."
+console.log(myName);
+
+var a = 10;
+var b = 5;
+var c = a + b 
+console.log(c)
+
+let firstname  = "Oluwafunto"
+let lastname = "Ayeni"
+
+let  fullname = "Oluwafunto" + " " + "Ayeni"
+console.log(fullname);
---
+++ js/new.js
@@ -0,0 +1,23 @@
+ function setup() {
+     var blue ="#1167b1"
+
+
+    createCanvas(800,600);
+    noStroke();
+
+    background(237, 210, 135)
+
+    fill(214, 41, 67);
+    rect(70, 60, 30, 90);
+
+    fill(200,0,0)
+    circle(40, 100, 50);
+
+    stroke(2);
+    line(0, 500, 200, 20)
+    
+}
+
+    
+
+    
