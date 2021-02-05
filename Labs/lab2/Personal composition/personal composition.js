abc8b2932d95baa8dfbcf09689a99802557d9acf
Oluwafunto <75193130+Oluwafunto@users.noreply.github.com>
2021-02-04 23:20:39 -0500

    Updated work

---
+++ Labs/lab2/Personal composition/personal composition.js
@@ -0,0 +1,58 @@
+function setup() {
+    var blue ="#1167b1"
+    var red = "#ED2939"
+
+
+   createCanvas(800,600);
+   noStroke();
+
+   background(blue)
+
+  
+
+   fill(255,253,55)
+   circle(100, 100, 70);
+
+   fill(red)
+   circle(70, 80, 10);
+
+   fill(red)
+   circle(130, 80, 10);
+
+   fill(red);
+   rect(55, 110, 90, 20);
+   
+   fill(218,165,32)
+   arc(250, 100, 200, 200, 150, 95)
+
+   fill(red)
+   circle(320, 50, 10);
+
+   fill(red)
+   circle(300, 100, 10);
+
+   fill(red)
+   circle(270, 100, 10);
+
+   fill(red)
+   circle(330, 75, 10);
+
+   fill(red)
+   circle(295, 75, 10);
+
+   fill(red)
+   circle(330, 100, 10);
+
+   fill(red)
+   circle(330, 125, 10);
+
+   fill(red)
+   circle(295, 125, 10);
+
+   fill(red)
+   circle(320, 150, 10);
+
+
+ 
+   
+}
